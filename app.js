c16d6f31fa7ee7ec8c82e5de18b57edf61c69cb7
Tim <s87462830@gmail.com>
2021-08-15 23:02:45 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 //Setting static files
 app.use(express.static('public'))
+//Setting body-parser 進行前置處理
+app.use(express.urlencoded({ extended: true}))
 
 //Routes setting:index
 app.get('/', (req, res) => {
@@ -30,19 +32,41 @@ app.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
-//Routes setting:show list
-app.get('/restaurants/:restaurants_id', (req, res) => {
-  //如使用Number(),當 req.params.restaurants_id是空字串時,restaurants_id為 0 的餐廳會被錯誤取出來
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurants_id)
-  res.render('show', { restaurant: restaurant })
+// //Routes setting:show list
+// app.get('/restaurants/:restaurants_id', (req, res) => {
+//   //如使用Number(),當 req.params.restaurants_id是空字串時,restaurants_id為 0 的餐廳會被錯誤取出來
+//   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurants_id)
+//   res.render('show', { restaurant: restaurant })
+// })
+// //Routes setting:search part
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword
+//   const restaurants = restaurantList.results.filter(restaurant => {
+//     return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+//   })
+//   res.render('index', { restaurants: restaurants, keyword: keyword })
+// })
+
+//Routes setting:create page
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
 })
-//Routes setting:search part
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+//Routes setting:catch create data
+app.post('/restaurants', (req, res) => {
+const restaurant = req.body
+return Restaurant.create({
+  name: restaurant.name,
+  name_en: restaurant.name_en,
+  category: restaurant.category,
+  image: restaurant.image || "https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5635/01.jpg",
+  location: restaurant.location,
+  phone: restaurant.phone,
+  google_map: restaurant.google_map,
+  rating: restaurant.rating,
+  description: restaurant.description,
   })
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+  .then(() => res.redirect('/'))
+  .catch(error => console.log(error))
 })
 
 //Activate and listen on the Express server
